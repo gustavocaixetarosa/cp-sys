58efc4d4c68ea7acfed0dc2e802885dd3211bba7
Gustavo Caixeta Rosa <gstvcaixeta@gmail.com>
2025-12-10 21:17:50 -0300

    fixing cors

--- backend/src/main/java/dev/gustavorosa/cobranca_cp/config/CorsConfig.java
+++ backend/src/main/java/dev/gustavorosa/cobranca_cp/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package dev.gustavorosa.cobranca_cp.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,23 +14,9 @@ public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-        .allowedOrigins(frontendUrl)
+        .allowedOriginPatterns(frontendUrl, "http://localhost:*", "http://127.0.0.1:*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
         .allowedHeaders("*")
         .allowCredentials(true);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
     }
 }
